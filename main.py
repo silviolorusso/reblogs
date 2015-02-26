a0140999ef06d2ca62d0965c911d173ef5788a22
Silvio Lorusso <s@silviolorusso.com>
2015-02-26 17:14:35 +0100

    report

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ my_width = 500
 my_height = 700
 
 # max amount of reblog pages
-my_max_pages = 1
+my_max_pages = 5
 
 # video speed
 my_speed = "12"
@@ -44,14 +44,29 @@ def main(width, height, max_pages, speed, source_url):
 	# get permalinks
 	urls = get_reblog_urls(source_url, max_pages)
 
-	# make text file
-
 	# create and go into folder
 	directory = source_url
 	directory = validate(directory)
 	if not os.path.exists(directory):
 		os.makedirs(directory)
 	os.chdir(directory)
+
+	# make report
+	urls_string = ""
+	for url in urls:
+		urls_string += url + "\n"
+
+	report_text = '''
+Source post: %s
+
+Amount of reblogs: %s
+
+Reblog URLS:
+%s
+	''' % (source_url, len(urls), urls_string)
+	report = open("report.txt", "w")
+	report.write(report_text)
+	report.close()
 
 	# create screenshot folder
 	if not os.path.exists('screenshots'):
@@ -69,7 +84,8 @@ def main(width, height, max_pages, speed, source_url):
 
 	# make video
 	# ffmpeg -f image2 -r 12 -pattern_type glob -i '*.png' -vcodec mpeg4 -y movie.mp4
-	subprocess.call(["ffmpeg", "-f", "image2", "-r", "12", "-pattern_type", "glob", "-i", "*.png", "-vcodec", "mpeg4", "-y", "../movie.mp4"])
+	movie_filename = "../movie_" + str(width) + "_" + str(height) + ".mp4" 
+	subprocess.call(["ffmpeg", "-f", "image2", "-r", "12", "-pattern_type", "glob", "-i", "*.png", "-vcodec", "mpeg4", "-y", movie_filename])
 	print "\nVideo successfully saved."
 
 # WORK
